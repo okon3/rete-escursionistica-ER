2c3bd7afbf0a68b31d873f1bf8a614ada6095084
Luca Regazzi <luca.regazzi@gmail.com>
2016-12-02 21:45:05 +0100

    Switch to wms

--- js/script.js
+++ js/script.js
@@ -21,7 +21,16 @@ var reerMap = {
     options : {
         baseLayerUrl : "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         defaultZoom : 8,
-        defaultCenter: [44.45534933372025, 10.5413818359375]
+        defaultCenter: [44.45534933372025, 10.5413818359375],
+        tileOptions : {
+            maxZoom: 20,  // max zoom to preserve detail on
+            tolerance: 5, // simplification tolerance (higher means simpler)
+            extent: 4096, // tile extent (both width and height)
+            buffer: 64,   // tile buffer on each side
+            debug: 0,      // logging level (0 to disable, 1 or 2)
+            indexMaxZoom: 0,        // max zoom in the initial tile index
+            indexMaxPoints: 100000, // max number of points per tile in the index
+        }
     },
     map : {},
     baseLayer : {},
@@ -34,6 +43,7 @@ var reerMap = {
     init: function(){
 		reerMap.initEvents();
 		reerMap.initMap();
+        pad = 0;
 	},
     initEvents : function(){
         $("#sidebar-toggle").click(function(e) {
@@ -71,14 +81,29 @@ var reerMap = {
             reerMap.layers[layer] = {};
 
             console.log("Loading layer:" + layer);
-            $.getJSON("data/"+ layer +".topo.json")
-                .done(function(data) {
-                    console.log("Loaded layer:" + layer);
-                    reerMap.layers[layer].layer = new L.TopoJSON(data, {style : reerMap.style});
-                    reerMap.layers[layer].enabled = false;
-                    $("#" + layer).removeClass("loading");
-                    reerMap.toggleLayer(layer);
-                });
+            reerMap.layers[layer].layer = L.tileLayer.wms('http://185.122.59.236:8080/geoserver/reer/wms?',
+                {
+                    layers : 'reer',
+                    transparent: true,
+                    format: 'image/png8',
+                }
+            );
+            reerMap.layers[layer].enabled = false;
+            reerMap.toggleLayer(layer);
+
+
+            // $.getJSON("data/"+ layer +".json")
+            //     .done(function(data) {
+            //         console.log("Loaded layer:" + layer);
+            //         reerMap.layers[layer].layer = L.canvasTiles()
+            //           .params({ debug: false, padding: 5 })
+            //           .drawing(reerMap.drawingOnCanvas);
+            //         //reerMap.layers[layer].layer = new L.TopoJSON(data, {style : reerMap.style});
+            //         tileIndex = geojsonvt(data, reerMap.options.tileOptions);
+            //         reerMap.layers[layer].enabled = false;
+            //         $("#" + layer).removeClass("loading");
+            //         reerMap.toggleLayer(layer);
+            //     });
         }else{
             reerMap.toggleLayer(layer);
         }
@@ -95,6 +120,68 @@ var reerMap = {
             reerMap.map.addLayer(reerMap.layers[layer].layer);
             reerMap.layers[layer].enabled = true;
         }
-        
-    }
+    },
+    drawingOnCanvas : function(canvasOverlay, params) {
+
+            var bounds = params.bounds;
+            params.tilePoint.z = params.zoom;
+
+            var ctx = params.canvas.getContext('2d');
+            ctx.globalCompositeOperation = 'source-over';
+
+
+            console.log('getting tile z' + params.tilePoint.z + '-' + params.tilePoint.x + '-' + params.tilePoint.y);
+
+            var tile = tileIndex.getTile(params.tilePoint.z, params.tilePoint.x, params.tilePoint.y);
+            if (!tile) {
+                console.log('tile empty');
+                return;
+            }
+
+            ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
+
+            var features = tile.features;
+
+            ctx.strokeStyle = 'red';
+            ctx.lineWidth = 2;
+
+
+            for (var i = 0; i < features.length; i++) {
+                var feature = features[i],
+                    type = feature.type;
+
+                ctx.fillStyle = feature.tags.color ? feature.tags.color : 'rgba(255,0,0,0.05)';
+                ctx.beginPath();
+
+                for (var j = 0; j < feature.geometry.length; j++) {
+                    var geom = feature.geometry[j];
+
+                    if (type === 1) {
+                        ctx.arc(geom[0] * ratio + pad, geom[1] * ratio + pad, 2, 0, 2 * Math.PI, false);
+                        continue;
+                    }
+
+                    for (var k = 0; k < geom.length; k++) {
+                        var p = geom[k];
+                        var extent = 4096;
+                       
+                        var x = p[0] / extent * 256;
+                        var y = p[1] / extent * 256;
+                        if (k) ctx.lineTo(x  + pad, y   + pad);
+                        else ctx.moveTo(x  + pad, y  + pad);
+                    }
+                }
+
+                if (type === 3 || type === 1) ctx.fill('evenodd');
+                ctx.stroke();
+            }
+        },
+        colorizeFeatures : function(data) {
+            var counter = 0;
+            for (var i = 0; i < data.features.length; i++) {
+                data.features[i].properties.color = '#ff0000';
+                counter += data.features[i].geometry.coordinates[0].length;
+            }
+            return counter;
+        }
 }
